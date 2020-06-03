9aa6fea47fb6194ae5d83ef9d6d060d3be49fe8a
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-06-03 09:20:02 -0400

    Adding dotenv and a route for root

--- bot.js
+++ bot.js
@@ -1,8 +1,14 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const Twitter = require("twitter");
+
+app.get("/", (req, res) => res.send("Hello!"));
 
 app.listen(port, () => {
   const client = new Twitter({
